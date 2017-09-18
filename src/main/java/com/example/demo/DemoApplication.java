e5764916a950a5e3aacfc547a1fd4aa6da954f30
wenism <whyeow@gmail.com>
2017-09-19 08:37:46 +1000

    With config.key1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,24 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+  @Value("${config.key1}")
+  String name = "World";
+  
   @RequestMapping("/")
   public String home() {
-      return "Hello World!";
+      return "Hello " + name;
   }
     
 	public static void main(String[] args) {
