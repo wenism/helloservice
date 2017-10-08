0d4621e51b8089bd1fb85a9833ed5c910a43cf87
wenism <whyeow@gmail.com>
2017-10-08 22:30:01 +1100

    Test hierarchy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 @RestController
 public class DemoApplication {
 
-  @Value("${config.key1}")
+  @Value("${key1}")
   String name = "World";
   
   @RequestMapping("/")
